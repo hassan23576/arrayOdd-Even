9f168c3308bf4caedb0230b6e7a20c7330ba049e
hassanbhuiyan <hassanbhuiyan@aol.com>
2020-01-10 21:35:27 -0500

    array

---
+++ src/array/Array1D.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Array1D {
+
+    public static void main(String[] args) {
+
+        int[] d = new int[6];
+        d[0] = 1;
+        d[1] = 2;
+        d[2] = 3;
+        d[3] = 4;
+        d[4] = 5;
+        d[5] = 6;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(d[i]);
+
+
+        }
+
+    }
+}
